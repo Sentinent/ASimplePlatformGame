0784ba34f78434c46f71696b257dc3baffb5ce87
Sentinent <MonkeyzFtw@gmail.com>
2015-11-05 19:34:57 -0800

    Added increasing timer speed and score functionality, game is mostly functional

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -14,9 +14,15 @@ import java.util.TimerTask;
 
 public class MainWindow extends JFrame implements KeyListener
 {
+	Timer gameTimer = new Timer();
+	
 	Random randomNumber = new Random();
 	
 	int timeUntilNextGenerate = 0; //Used for creating a gap between blocks
+	int score = 0; //Currents score, adds for every 5 game ticks
+	int ticksTillNextScore = 5; //Used to measure every 5 game ticks
+	int gameTickSpeed = 200; //Default value is 200ms, decreases by 10 ms every 20 game ticks
+	int ticksTillNextSpeed = 20; //Counts 20 game ticks to increase update speed
 	
 	Player player = new Player(162);
 	
@@ -25,38 +31,58 @@ public class MainWindow extends JFrame implements KeyListener
 	JPanel Ground = new JPanel();
 	JPanel GameWindow = new JPanel();
 	
+	JLabel scoreLabel = new JLabel("0");
+	
 	JLabel[] positions = new JLabel[181];
 	
 	
 	public MainWindow()
 	{
 		//Adds the timer to call Update()
-		Timer gameTimer = new Timer();
 		gameTimer.scheduleAtFixedRate(new TimerTask()
-				{
-					@Override
-					public void run()
-					{
-						
-						UpdateGame();	
-						GenerateBlocks();
-					}
-				}, 200, 200);  //Creates a new timer that calls Update() every half second
+		{
+			@Override
+			public void run()
+			{
+				DoGameTick(); //Handles everything that depends on game ticks
+				UpdateGame(); //Updates players and blocks
+				GenerateBlocks(); //Creates new blocks
+			}
+		}, 200, 200);  //Creates a new timer that calls Update() every half second
 		
 		this.addKeyListener(this);
 		this.setLayout(new BorderLayout()); 
 				
 		initGame();
 		
-		this.add(GameWindow, BorderLayout.NORTH);
+		this.add(GameWindow, BorderLayout.CENTER);
 		this.add(Ground, BorderLayout.SOUTH);
 		
 		//this.setSize(800, 400);
+		this.setTitle("A real game!");
 		this.pack();
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
 		this.setVisible(true);
 	}
+	
+	public void updateTimer()
+	{
+		gameTimer.cancel();
+		gameTimer = null; //Deletes the running instance of the TimerTask
+		gameTimer = new Timer();
+		gameTimer.scheduleAtFixedRate(new TimerTask()
+		{
+			@Override
+			public void run()
+			{
+				DoGameTick(); //Handles everything that depends on game ticks
+				UpdateGame(); //Updates players and blocks
+				GenerateBlocks(); //Creates new blocks
+			}
+		}, gameTickSpeed, gameTickSpeed);
+	}
+	
 	
 	public void initGame()
 	{
@@ -67,6 +93,12 @@ public class MainWindow extends JFrame implements KeyListener
 			positions[i] = new JLabel("" + i);
 			GameWindow.add(positions[i]);
 		}
+		
+		//Adds the score counter in the top right
+		{
+			positions[19].setText("0");
+		}
+		
 		//Adds the player
 		positions[player.position].setText("O");
 		
@@ -74,6 +106,36 @@ public class MainWindow extends JFrame implements KeyListener
 		JLabel ground = new JLabel("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
 		Ground.add(ground);
 		this.add(Ground);
+		
+	}
+	
+	public void DoGameTick()
+	{
+		if (ticksTillNextScore == 0) //We add one to score
+		{
+			score += 1;
+			ticksTillNextScore = 5;
+			scoreLabel.setText("" + score); //We put it here to not take up timer efficiency
+		}
+		else
+		{
+			ticksTillNextScore -= 1; //If it is not 0, decrease it by one
+		}
+		
+		if (gameTickSpeed <= 80) //Can't keep up if it updates every 80 milliseconds
+		{
+			return;
+		}
+		if (ticksTillNextSpeed == 0)
+		{
+			gameTickSpeed -= 10; //Subtract 10 ms from the update speed
+			ticksTillNextSpeed = 30; //Increases the ticksTillNextSpeed
+			updateTimer();
+		}
+		else
+		{
+			ticksTillNextSpeed -= 1;
+		}		
 	}
 	
 	public void GenerateBlocks()
@@ -95,12 +157,10 @@ public class MainWindow extends JFrame implements KeyListener
 			{
 				if (blocks[currentBlockChecker] == null)
 				{
-					System.out.println(currentBlockChecker + " is null");
 					emptyBlocks.add(currentBlockChecker);
 				}
 				else if (!blocks[currentBlockChecker].isActive) //If the block is not in use
-				{
-					System.out.println(currentBlockChecker + " is active: " + blocks[currentBlockChecker].isActive);
+				{;
 					emptyBlocks.add(currentBlockChecker);  //Holds all the positions of the not used blocks
 				}
 				currentBlockChecker += 1;
@@ -109,7 +169,6 @@ public class MainWindow extends JFrame implements KeyListener
 			int height = 0; //For stacking blocks on top of each other
 			for (int blockPosition : emptyBlocks) //For each empty block position...
 			{
-				System.out.println("Empty block position is: " + blockPosition);
 				blocks[blockPosition] = new Block(179 - (height * 20));
 				blocks[blockPosition].isActive = true;
 				height += 1;
@@ -127,6 +186,8 @@ public class MainWindow extends JFrame implements KeyListener
 		{
 			positions[i].setText("");
 		}
+		//Updates the score
+		positions[19].setText("" + score); //Sets the top right to score
 		
 		//Updates player position
 		if (player.firstJump)
@@ -136,7 +197,7 @@ public class MainWindow extends JFrame implements KeyListener
 		else if (player.isComingDown)
 		{
 			player.position += 20;
-			if (player.position <= 92           )
+			if (player.position <= 92)
 			{
 				player.isComingDown = true;
 			}
@@ -186,6 +247,7 @@ public class MainWindow extends JFrame implements KeyListener
 					if (blocks[i].position == player.position)
 					{
 						System.out.println("You lost!");
+						System.out.println("Your final score was " + score + "!");
 						System.exit(1);
 					}
 				}								
@@ -197,11 +259,8 @@ public class MainWindow extends JFrame implements KeyListener
 	public void keyPressed(KeyEvent e) 
 	{
 		if (e.getKeyCode() == 38 || e.getKeyCode() == 32)  // 38 is up arrow, and 32 is space bar
-		{
-			System.out.println("Jumped!");
-			
-			player.Jump();
-			
+		{			
+			player.Jump();			
 		}
 	}
 	
